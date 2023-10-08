eef70c011844106c8e12147d8ce583a3e3029be8
Logoman1995 <david.grigorian777@gmail.com>
2023-10-08 18:58:14 +0400

    make an object from an array

---
+++ cycle/ex11.js
@@ -0,0 +1,9 @@
+// Дан следующий массив сделать значениями объекта.
+
+
+let arr = ['a', 'b', 'c', 'd', 'e'];
+let obj = {};
+for (let i = 1; i <= arr.length; i++) {
+obj[i] = arr[i-1];
+}
+console.log(obj);
