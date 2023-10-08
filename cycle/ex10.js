89230eada5ca6adfcb13affeca9659e87d421caa
Logoman1995 <david.grigorian777@gmail.com>
2023-10-08 18:54:10 +0400

    find the sum of the keys of this object and divide if by the sum of the values

---
+++ cycle/ex10.js
@@ -0,0 +1,10 @@
+// Дан следующий объект.Найдите сумму ключей этого объекта и поделите ее на сумму значений.
+let obj = {1: 6, 2: 7, 3: 8, 4: 9, 5: 10};
+let sum = 0;
+let sum2 = 0;
+for (let key in obj) {
+sum += Number(key);
+sum2 += new Number(obj[key]);
+}
+
+console.log(sum/sum2);
