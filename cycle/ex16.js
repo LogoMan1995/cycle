932437ffcb9b6d7d093e7ab8c6470ed10654ef16
Logoman1995 <david.grigorian777@gmail.com>
2023-10-09 13:42:09 +0400

    An array with numbers is given. The numbers that are divisible by 2 will be squared , аnd the numbers that are divisible by 3 will be cubed

---
+++ cycle/ex16.js
@@ -0,0 +1,17 @@
+// Дан массив с числами. Перебрать его циклом и числа, которые делятся на 2, возведите в квадрат, а числа, которые делятся на 3, возведите в куб.
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+for (let elem of arr) {
+	let result;
+	
+	if (elem % 2 == 0) {
+		result = elem * elem;
+	} else if (elem % 3 == 0) {
+		result = elem * elem * elem;
+	} else {
+		continue; 
+	}
+	
+	console.log(result); 
+}
